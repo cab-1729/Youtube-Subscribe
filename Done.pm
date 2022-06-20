2b86bbcb4d8bd791ae52cccd923f77a23f462e8e
cab-1729 <60106638+cab-1729@users.noreply.github.com>
2022-06-20 08:40:35 +0530

    Add files via upload

---
+++ Done.pm
@@ -0,0 +1,28 @@
+package Done;
+#This file may change when code is run
+use strict;
+use warnings;
+our sub new{
+	my $class=shift;
+	our $self={
+		'UUXIJgqnII2ZOINSWNOGFThA'=>'7EvXMMRDqeI',
+		'UUupvZG-5ko_eiXAupbDfxWw'=>'ZuWKEskal6A',
+		'UUaXkIU1QidjPwiAYu6GcHjg'=>'kCFYCB0e_i8',
+		'UUGy6uV7yqGWDeUWTZzT3ZEg'=>'CTH7kRkVxWM',
+		'PL0lo9MOBetEHhfG9vJzVCTiDYcbhAiEqL'=>'HwrPhOp6-aM',
+		'PL5--8gKSku16ZCPdQ1RpLW53zXpPlCJ84'=>'demtnVPJgXE',
+		'UU8butISFwT-Wl7EV0hUK0BQ'=>'VtRLrQ3Ev-U',
+		'UUXuqSBlHAE6Xw-yeJA0Tunw'=>'ugtntRVMqFo',
+		'UU7YOGHUfC1Tb6E4pudI9STA'=>'uAutGsrEWSw',
+		'UUElqfal0wzzpLsHlRuqZjaA'=>'Oondd6gZ8NY',
+		'UUeM9I-20oWUs8daIIpsNHoQ'=>'JvRUuz_qy3E',
+		'UUxPeEcH0xaCK9nBK98EFhDg'=>'xko3Dj5h6-o',
+		'PLO0l2rQLwh0kM34p1jugDSe4b2cy_X2Bv'=>'hCE57uKxKQ4',
+		'UUmC_eIBo4e4KDGJzdi57r9g'=>'nGk8r86GJf0',
+		'UUeUJFQ0D9qs6aVNyUt9fkeQ'=>'MSo9iBYnaEI',
+		'UUvPXiKxH-eH9xq-80vpgmKQ'=>'zETNKkECocg',
+	};
+	bless $self,$class;
+	return $self;
+}
+1;
---
+++ Subscribed.pm
@@ -0,0 +1,51 @@
+#Disclaimer: This file is made for illustrative purposes only. It is purely fictional and does not represent the preferences and opinions of the author. Any similarities with configurations of other people in real life are coincidental. This project does not endorse any of the Youtube channels mentioned.
+package Subscribed;
+use warnings;
+use strict;
+our sub new{
+	my $class=shift;
+	#Each topic is a data member
+	our $self={
+		#Each playlist is an array inside the topic
+		News=>[
+			["Fox News","UUXIJgqnII2ZOINSWNOGFThA"],#channel id with leading UC replaced by UU
+			["CNN","UUupvZG-5ko_eiXAupbDfxWw"],
+			["MSNBC","UUaXkIU1QidjPwiAYu6GcHjg"],
+			["Rebel News","UUGy6uV7yqGWDeUWTZzT3ZEg","$ENV{HOME}/Videos/Rebel News"],#videos will be downloaded to '~/Videos/Rebel News'
+		],
+		Technology=>[
+			["Github Foundation","PL0lo9MOBetEHhfG9vJzVCTiDYcbhAiEqL"],
+			["Hey DT!!","PL5--8gKSku16ZCPdQ1RpLW53zXpPlCJ84"],
+			["freeCodeCamp","UU8butISFwT-Wl7EV0hUK0BQ"],
+			["Linus Tech Tips","UUXuqSBlHAE6Xw-yeJA0Tunw"],
+			["Live Overflow","UU7YOGHUfC1Tb6E4pudI9STA"],
+		],
+		Politics=>[
+			["Alexandria Ocasio-Cortez","UUElqfal0wzzpLsHlRuqZjaA"],
+			["Senator Rand Paul","UUeM9I-20oWUs8daIIpsNHoQ"],
+			["Speaker Nancy Pelosi","UUxPeEcH0xaCK9nBK98EFhDg"],
+			["Hold These Truths - Dan Crenshaw","PLO0l2rQLwh0kM34p1jugDSe4b2cy_X2Bv"],
+		]
+		Music=>[
+			["Vevo","UUmC_eIBo4e4KDGJzdi57r9g","$ENV{HOME}/Music"],
+		],
+		History=>[
+			["The Armchair Historian","UUeUJFQ0D9qs6aVNyUt9fkeQ"],
+			["Epic History","UUvPXiKxH-eH9xq-80vpgmKQ"],
+		],
+	};
+	bless $self,$class;
+	return $self;
+}
+our sub api_key{
+	#This function returns your youtube api key.
+	#You are required to write this function
+	return "YOUR YOUTUBE API KEY";
+}
+our sub download{
+	#you may rewrite this function to suit your own ways of downloading yourtube videos
+	my ($id,$directory)=@_;
+	chdir "$directory";
+	system("yt-dlp --write-thumbnail --embed-chapters https://www.youtube.com/watch?v=$id");
+}
+1;
